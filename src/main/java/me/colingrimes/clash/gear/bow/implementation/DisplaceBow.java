e91e5d3c4af4b73f7080d490dd93833e6f3bd6d4
Scill <realscill@gmail.com>
2025-03-18 23:14:44 -0400

    Add Displace bow

--- src/main/java/me/colingrimes/clash/gear/bow/implementation/DisplaceBow.java
+++ src/main/java/me/colingrimes/clash/gear/bow/implementation/DisplaceBow.java
@@ -2,19 +2,26 @@ package me.colingrimes.clash.gear.bow.implementation;
 
 import me.colingrimes.clash.Clash;
 import me.colingrimes.clash.config.GearSettings;
+import me.colingrimes.clash.gear.bow.BowInfo;
 import me.colingrimes.clash.gear.util.GearGrade;
 import me.colingrimes.clash.gear.bow.BowGear;
+import org.bukkit.Location;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import javax.annotation.Nonnull;
 
 public class DisplaceBow extends BowGear {
 
 	public DisplaceBow(@Nonnull Clash plugin) {
-		super(plugin, "displace", GearSettings.BOW_DISPLACE_NAME, GearSettings.BOW_DISPLACE_DESC, GearGrade.C);
+		super(plugin, "displace", GearSettings.BOW_DISPLACE_NAME, GearSettings.BOW_DISPLACE_DESC, GearGrade.D);
 	}
 
 	@Override
-	public boolean isEnabled() {
-		return false;
+	public boolean activate(@Nonnull EntityDamageByEntityEvent event, @Nonnull BowInfo bow) {
+		Location shooterLocation = bow.shooterLocation();
+		Location damagedLocation = bow.damaged().getLocation();
+		bow.shooter().teleport(damagedLocation);
+		bow.damaged().teleport(shooterLocation);
+		return true;
 	}
 }
