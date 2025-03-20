17045fed553c0050e8d6caaa26f5d233bda26bb6
Scill <realscill@gmail.com>
2025-03-19 21:17:01 -0400

    Clean up gear show GUI

--- src/main/java/me/colingrimes/clash/command/gear/show/GearShow.java
+++ src/main/java/me/colingrimes/clash/command/gear/show/GearShow.java
@@ -1,22 +1,47 @@
 package me.colingrimes.clash.command.gear.show;
 
+import me.colingrimes.clash.gear.bow.BowGear;
 import me.colingrimes.midnight.command.Command;
 import me.colingrimes.midnight.command.handler.util.ArgumentList;
 import me.colingrimes.midnight.command.handler.util.CommandProperties;
 import me.colingrimes.midnight.command.handler.util.Sender;
 import me.colingrimes.clash.Clash;
+import me.colingrimes.midnight.menu.Gui;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 public class GearShow implements Command<Clash> {
 
 	@Override
 	public void execute(@Nonnull Clash plugin, @Nonnull Sender sender, @Nonnull ArgumentList args) {
-		new GearGui(plugin, sender.player()).open();
+		new GearShowGui(plugin, sender.player()).open();
 	}
 
 	@Override
 	public void configureProperties(@Nonnull CommandProperties properties) {
 		properties.setPlayerRequired(true);
+	}
+
+	private static class GearShowGui extends Gui {
+		private final Clash plugin;
+		private final Player player;
+
+		public GearShowGui(@Nonnull Clash plugin, @Nonnull Player player) {
+			super(player, "Gear", (int) Math.ceil(plugin.getBows().size() / 9.0));
+			this.plugin = plugin;
+			this.player = player;
+		}
+
+		@Override
+		public void draw() {
+			List<BowGear> bows = plugin.getBows();
+			for (int i=0; i<bows.size(); i++) {
+				BowGear bow = bows.get(i);
+				getSlot(i).setItem(bow.getGear()).bind(ClickType.LEFT, __ -> player.getInventory().addItem(bow.getGear()));
+			}
+		}
 	}
 }
