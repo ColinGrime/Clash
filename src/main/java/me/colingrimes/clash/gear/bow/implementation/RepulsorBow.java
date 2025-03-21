cc13eb192147e84e2a50ffaba5bb09cfe8abeb1a
Scill <realscill@gmail.com>
2025-03-20 23:26:48 -0400

    Add basic Repulsor bow

--- src/main/java/me/colingrimes/clash/gear/bow/implementation/RepulsorBow.java
+++ src/main/java/me/colingrimes/clash/gear/bow/implementation/RepulsorBow.java
@@ -2,8 +2,15 @@ package me.colingrimes.clash.gear.bow.implementation;
 
 import me.colingrimes.clash.Clash;
 import me.colingrimes.clash.config.GearSettings;
+import me.colingrimes.clash.gear.bow.BowInfo;
 import me.colingrimes.clash.gear.util.GearGrade;
 import me.colingrimes.clash.gear.bow.BowGear;
+import me.colingrimes.midnight.util.bukkit.Entities;
+import me.colingrimes.midnight.util.bukkit.Vectors;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 
 import javax.annotation.Nonnull;
 
@@ -14,7 +21,17 @@ public class RepulsorBow extends BowGear {
 	}
 
 	@Override
-	public boolean isEnabled() {
+	public boolean activate(@Nonnull ProjectileHitEvent event, @Nonnull BowInfo bow) {
+		bow.world().playSound(bow.arrowLocation(), Sound.ENTITY_GENERIC_EXPLODE, 1f, 1f);
+		for (Entity entity : Entities.nearby(bow.arrowLocation(), 10)) {
+			entity.setVelocity(Vectors.direction(bow.arrowLocation().clone().subtract(0, 1, 0), entity.getLocation()).multiply(2));
+		}
+		return true;
+	}
+
+	@Override
+	public boolean activate(@Nonnull EntityDamageByEntityEvent event, @Nonnull BowInfo bow) {
+		event.setCancelled(true);
 		return false;
 	}
 }
