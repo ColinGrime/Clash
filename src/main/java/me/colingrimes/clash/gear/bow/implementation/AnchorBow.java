234d6471655c3cbf8115c407f52daa2e8ef364b0
Scill <realscill@gmail.com>
2025-03-20 23:26:25 -0400

    Clean up Anchor bow logic

--- src/main/java/me/colingrimes/clash/gear/bow/implementation/AnchorBow.java
+++ src/main/java/me/colingrimes/clash/gear/bow/implementation/AnchorBow.java
@@ -7,7 +7,6 @@ import me.colingrimes.clash.gear.bow.BowGear;
 import me.colingrimes.clash.gear.bow.BowInfo;
 import me.colingrimes.midnight.scheduler.Scheduler;
 import me.colingrimes.midnight.util.bukkit.Entities;
-import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.ProjectileHitEvent;
@@ -28,20 +27,10 @@ public class AnchorBow extends BowGear {
 
 	@Override
 	public boolean activate(@Nonnull ProjectileHitEvent event, @Nonnull BowInfo bow) {
-		Location location;
-		if (event.getHitBlock() != null) {
-			location = event.getHitBlock().getLocation();
-		} else if (event.getHitEntity() != null) {
-			location = event.getHitEntity().getLocation();
-		} else {
-			return false;
-		}
-
-		Location anchorLocation = location.clone().add(0, 1, 0);
 		Scheduler.sync().runRepeating(() -> {
-			Entities.spawn(EntityType.EVOKER_FANGS, anchorLocation);
-			for (LivingEntity entity : Entities.nearby(LivingEntity.class, anchorLocation, 3)) {
-				entity.teleport(anchorLocation.setDirection(entity.getLocation().getDirection()));
+			Entities.spawn(EntityType.EVOKER_FANGS, bow.arrowLocation());
+			for (LivingEntity entity : Entities.nearby(LivingEntity.class, bow.arrowLocation(), 3)) {
+				entity.teleport(bow.arrowLocation().setDirection(entity.getLocation().getDirection()));
 				entity.damage(1);
 			}
 		}, 0L, 10L, 5 * 20L);
